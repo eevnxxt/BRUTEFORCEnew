44ef9155be97b50510e1959e57699bdb8a47a2aa
eevnxxt <50643099+eevnxxt@users.noreply.github.com>
2019-05-15 06:51:54 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -7,19 +7,19 @@ cy='\033[36;1m' #cyan
 me='\033[31;1m' #merah
 pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
-# Jangan Recode Ya gayn
+#SUBSCRIBE YT CHANNEL BARU : NOOB BWANG
 echo
 echo
 echo
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
-echo $i" |"$cy"     INDONESIA DARK TERMUX ASSOCIATE"$i"         |"
+echo $i" |"$cy"     ANON 77 CYBER TERMUX"$i"                         |"
 echo $i" |"$me"─────────────────────────────────────────────"$i"|"
-echo $i" |"$pu" AUTHOR :"$ku" RISKI DARMAWAN  [FR13ND8]"$i"          |"
-echo $i" |"$pu" FACEBOOK :"$ku" Riski Darmawan"$i"                   |"
-echo $i" |"$pu" THANKS TO :"$cy"~ [MR.P3RC1L] [MR.S∆MAx] [MR.B]"$i"  |"
-echo $i" |"$cy"            ~ [cyber] [%MR.S%] [AldykaPrygh]"$i" |"
-echo $i" |"$pu" Contack WhatsApp :"$ku" 085835787069"$i"             |"
-echo $i" |"$pu" Contack Gmail :"$ku" dermawan0858@gmail.com"$i"      |"
+echo $i" |"$pu" AUTHOR :"$ku"NOOB BWANG[FR13ND8]"$i"                 |"
+echo $i" |"$pu" YOUTUBE :"$ku"NOOB BWANG"$i"                         |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.X] [MR.G] [MR.T]"$i"           |"
+echo $i" |"$cy"            ~ [cyberTEAM] [MR.ZTT] [MR.XxX]"$i"       |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 081285939588"$i"             |"
+echo $i" |"$pu" Contack Gmail :"$ku" nabilah1355@gmail.com@gmail.com"$i"|"
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo
 echo $i"────────────────────────────────    ─────"
@@ -43,7 +43,7 @@ echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ────�
 echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" K"$i" |"
 echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" E"$i" |"
 echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
-echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU       "$i"|"$i"    |"$cy" E"$i" |"
 echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" N"$i" |"
 echo $i"────────────────────────────────    ─────"
 echo
